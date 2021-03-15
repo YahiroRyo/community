c76e2005e0ebd1afffd0fef39cfc21464b6b44f9
YahiroRyo <honakafn@gmail.com>
2021-03-15 21:09:23 +0900

    プラグインをインポート 基本的な構成のhtmlをtemplate内に実装

--- main/resources/js/App.vue
+++ main/resources/js/App.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
-        
-    </div>
+    <link rel="stylesheet" href="/css/base.css">
+    <header id="header">
+    </header>
+    <main>
+    </main>
+    <footer id="footer">
+    </footer>
 </template>
 
 <script>
+    import ***REMOVED*** reactive ***REMOVED*** from 'vue'
+    import ***REMOVED*** useStore ***REMOVED*** from 'vuex'
+    import ***REMOVED*** useRouter, useRoute ***REMOVED*** from 'vue-router'
+
     export default ***REMOVED***
-        
+        setup() ***REMOVED***
+            const data = reactive(***REMOVED***
+                store: useStore(),
+                router: useRouter(),
+                route: useRoute(),
+            ***REMOVED***)
+            return ***REMOVED*** data ***REMOVED***
+        ***REMOVED***
     ***REMOVED***
 </script>
