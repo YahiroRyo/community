b08228da2462bfb596e25a90f25279317bf59fa8
YahiroRyo <honakafn@gmail.com>
2021-03-15 20:37:00 +0900

    storeとrouterをインポート

--- main/resources/js/app.js
+++ main/resources/js/app.js
@@ -1,5 +1,9 @@
 import ***REMOVED*** createApp ***REMOVED*** from 'vue'
+import ***REMOVED*** store ***REMOVED*** from './store'
+import ***REMOVED*** router ***REMOVED*** from './router'
 import App from './App.vue'
 
 let app = createApp(App)
+app.use(store)
+app.use(router)
 app.mount('#app')
