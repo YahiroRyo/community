2e83b9c89972b9e37053c1149de8b2fba7492bbd
YahiroRyo <honakafn@gmail.com>
2021-03-15 18:16:48 +0900

    vueのファイルを表示するテンプレートを作成

---
+++ main/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="" />
+  <title>community</title>
+  <script src="***REMOVED******REMOVED*** mix('../js/app.js') ***REMOVED******REMOVED***" defer></script>
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
