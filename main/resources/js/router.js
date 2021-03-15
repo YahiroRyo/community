be9a83309f6707d236b3846aa77c3cfa61e7820a
YahiroRyo <honakafn@gmail.com>
2021-03-15 20:39:02 +0900

    vue-routerをインストールしたためrouter.jsを作成

---
+++ main/resources/js/router.js
@@ -0,0 +1,11 @@
+import ***REMOVED*** createWebHistory, createRouter ***REMOVED*** from "vue-router";
+
+const routes = [
+];
+  
+const router = createRouter(***REMOVED***
+  history: createWebHistory(),
+  routes,
+***REMOVED***);
+
+export default router;
