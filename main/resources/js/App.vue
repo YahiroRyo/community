f39ca54342023c46fb8323a4743365f345d0fafd
YahiroRyo <honakafn@gmail.com>
2021-03-17 22:07:01 +0900

    サイドに表示するhtmlを実装、window__contentにモジュールを追加、モジュールをインポート

--- main/resources/js/App.vue
+++ main/resources/js/App.vue
@@ -1,40 +1,65 @@
 <template>
     <link rel="stylesheet" href="/css/base.css">
     <header id="header">
+        <img class="header__icon" src="/images/icon.svg">
     </header>
-    <main style="height: 100vh">
-        <!-- テストしているモジュールのフラグをtrueにするボタン -->
-        <div style="width: 300px; height: 300px; background-color: #eee;" @click="data.testTrigger = true"><p>Test btn trigger</p></div>
-        
-        <!-- 背景を暗くする -->
-        <transition name="dark-background-anim"><div v-show="$store.state.window.use" class="dark-background"></div></transition>
-        
-        <!-- window module -->
-        <transition name="window-anim">
-            <!-- onMounted時に表示するコンポーネントがwindowの情報を指定できるようにするため、v-ifを使用 -->
-            <div v-if="$store.state.window.use" :style="`width: ${$store.state.window.width}px; height: ${$store.state.window.height}px;`" class="window">
-                <img @click="$store.state.window.use = false" class="window__close-btn" src="/images/materials/close.svg">
-                <h1 class="window__title">{{$store.state.window.title}}</h1>
-                <!-- window__content内のコンポーネントは予め用意しておき、if文で切り替える -->
-                <div class="window__content" :style="`height: ${data.window.contentHeight}px;`">
-                    <!-- 使いたいコンポーネントに番号を定義し、if文で表示させる -->
-                    <WindowExample v-if="$store.state.window.currentComponent === 0" />
+    <div id="container">
+        <nav class="menu">
+            <ul class="menu__item-wapper">
+                <li>
+                    <router-link to="/" class="menu__item">グローバルな投稿を閲覧</router-link>
+                </li>
+                <li>
+                    <router-link to="/" class="menu__item">プロフィール</router-link>
+                </li>
+                <li>
+                    <router-link to="/" class="menu__item">コミュニティ</router-link>
+                </li>
+                <li>
+                    <router-link to="/" class="menu__item">お問い合わせ</router-link>
+                </li>
+                <li>
+                    <button @click="displayWindow(1)" class="menu__item">モジュール</button>
+                </li>
+                <li>
+                    <!-- テストしているモジュールのフラグをtrueにするボタン -->
+                    <button class="menu__item" style="color: red;" @click="data.testTrigger = true">Test Trigger</button>
+                </li>
+            </ul>
+        </nav>
+        <main id="main">
+            <!-- 背景を暗くする -->
+            <transition name="dark-background-anim"><div v-show="$store.state.window.use" class="dark-background"></div></transition>
+            <!-- window module -->
+            <transition name="window-anim">
+                <!-- onMounted時に表示するコンポーネントがwindowの情報を指定できるようにするため、v-ifを使用 -->
+                <div v-if="$store.state.window.use" :style="`width: ${$store.state.window.width}px; height: ${$store.state.window.height}px;`" class="window">
+                    <img @click="$store.state.window.use = false" class="window__close-btn" src="/images/materials/close.svg">
+                    <h1 class="window__title">{{$store.state.window.title}}</h1>
+                    <!-- window__content内のコンポーネントは予め用意しておき、if文で切り替える -->
+                    <div class="window__content" :style="`height: ${data.window.contentHeight}px;`">
+                        <!-- 使いたいコンポーネントに番号を定義し、if文で表示させる -->
+                        <WindowExample v-if="$store.state.window.currentComponent === 0" />
+                        <Modules v-else-if="$store.state.window.currentComponent === 1" />
+                    </div>
                 </div>
-            </div>
-        </transition>
-
-        <!-- alert module -->
-        <transition name="alerts-anim">
-            <div v-if="$store.state.alert.object" :class="
-                ($store.state.alert.object.type === 0) ?    'alert alert_success'       :
-                ($store.state.alert.object.type === 1) ?    'alert alert_attention'     : 'alert alert_danger'"
-                >
-                <p class="alert__content">{{$store.state.alert.object.content}}</p>
-            </div>
-        </transition>
-    </main>
-    <footer id="footer">
-    </footer>
+            </transition>
+            <!-- alert module -->
+            <transition name="alerts-anim">
+                <div v-if="$store.state.alert.object" :class="
+                    ($store.state.alert.object.type === 0) ?    'alert alert_success'       :
+                    ($store.state.alert.object.type === 1) ?    'alert alert_attention'     : 'alert alert_danger'"
+                    >
+                    <p class="alert__content">{{$store.state.alert.object.content}}</p>
+                </div>
+            </transition>
+        </main>
+        <div class="other">
+            <ul class="other__item-wapper">
+                <li class="other__item">投稿する</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -46,6 +71,7 @@
 
     /* ---------------コンポーネントインポート--------------- */
     import WindowExample from './components/WindowExampleComponent.vue'
+    import Modules from './components/Modules.vue'
 
     export default {
         components:{
@@ -53,7 +79,8 @@
 
 
             /* ---------------Window用コンポーネント--------------- */
-            WindowExample
+            WindowExample,
+            Modules,
         },
         setup() {
             const data = reactive({
@@ -62,7 +89,6 @@
                 route: useRoute(),
                 window: { contentHeight: 0, },
                 testTrigger: false,
-                u: 0,
             })
             watch(() => data.store.state.window.use, () => {
                 if (data.store.state.window.use) {
@@ -84,19 +110,17 @@
                     if (data.store.state.window.functions.close) { data.store.state.window.functions.close() }
                 }
             })
+            // 挙動を確かめるためのtestTriggerフラグをwatchしている
             watch(() => data.testTrigger, () => {
-                displayWindow(0)
                 if (data.testTrigger) {
-                    createAlert(
-                        new alert(
-                            'test Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alert'
-                            , 0
-                        )
-                    )
+                    //window
+                    displayWindow(0)
+                    //alert
+                    createAlert(new alert('alert', 0))
                     data.testTrigger = false
                 }
             })
-            return { data }
+            return { displayWindow, data }
         }
     }
 </script>
