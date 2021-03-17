30638917e5082efec031df2c6a2e6dfb8f7c2cbf
YahiroRyo <honakafn@gmail.com>
2021-03-17 15:46:13 +0900

    windowモジュールに使う変数を定義

--- main/resources/js/store.js
+++ main/resources/js/store.js
@@ -3,6 +3,17 @@ import ***REMOVED*** createStore ***REMOVED*** from 'vuex'
 export const store = createStore(***REMOVED***
     state() ***REMOVED***
       return ***REMOVED***
+        window: ***REMOVED***
+          currentComponent: 0,
+          title: 'タイトルが設定されていません',
+          width: 500,
+          height: 500,
+          use: false,
+          functions: ***REMOVED***
+            close: null,
+            open: null,
+          ***REMOVED***
+        ***REMOVED***,
       ***REMOVED***
     ***REMOVED***,
   ***REMOVED***)
