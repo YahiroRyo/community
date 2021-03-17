ab3046b0fb7e3026c98e8ccbcb4012ad3e2af44e
YahiroRyo <honakafn@gmail.com>
2021-03-17 18:23:37 +0900

    alertモジュールを表示するための仕組みを実装・HTMLに機能に対しての説明を追記

--- main/resources/js/App.vue
+++ main/resources/js/App.vue
@@ -4,10 +4,12 @@
     </header>
     <main style="height: 100vh">
         <!-- テストしているモジュールのフラグをtrueにするボタン -->
-        <div style="width: 300px; height: 300px; background-color: #eee;" @click="$store.state.window.use = true"><p>Test btn trigger</p></div>
-        <transition name="dark-background-anim">
-            <div v-show="$store.state.window.use" class="dark-background"></div>
-        </transition>
+        <div style="width: 300px; height: 300px; background-color: #eee;" @click="data.testTrigger = true"><p>Test btn trigger</p></div>
+        
+        <!-- 背景を暗くする -->
+        <transition name="dark-background-anim"><div v-show="$store.state.window.use" class="dark-background"></div></transition>
+        
+        <!-- window module -->
         <transition name="window-anim">
             <!-- onMounted時に表示するコンポーネントがwindowの情報を指定できるようにするため、v-ifを使用 -->
             <div v-if="$store.state.window.use" :style="`width: ${$store.state.window.width}px; height: ${$store.state.window.height}px;`" class="window">
@@ -20,6 +22,16 @@
                 </div>
             </div>
         </transition>
+
+        <!-- alert module -->
+        <transition name="alerts-anim">
+            <div v-if="$store.state.alert.object" :class="
+                ($store.state.alert.object.type === 0) ?    'alert alert_success'       :
+                ($store.state.alert.object.type === 1) ?    'alert alert_attention'     : 'alert alert_danger'"
+                >
+                <p class="alert__content">{{$store.state.alert.object.content}}</p>
+            </div>
+        </transition>
     </main>
     <footer id="footer">
     </footer>
@@ -30,6 +42,7 @@
     import { useStore } from 'vuex'
     import { useRouter, useRoute } from 'vue-router'
     import { window, createWindow } from './window';
+    import { alert, createAlert } from './alert';
 
     /* ---------------コンポーネントインポート--------------- */
     import WindowExample from './components/WindowExampleComponent.vue'
@@ -47,7 +60,9 @@
                 store: useStore(),
                 router: useRouter(),
                 route: useRoute(),
-                window: { contentHeight: 0, }
+                window: { contentHeight: 0, },
+                testTrigger: false,
+                u: 0,
             })
             watch(() => data.store.state.window.use, () => {
                 if (data.store.state.window.use) {
@@ -67,6 +82,17 @@
                     data.window.contentHeight = data.store.state.window.height - 120;
                 } else {
                     if (data.store.state.window.functions.close) { data.store.state.window.functions.close() }
+                }
+            })
+            watch(() => data.testTrigger, () => {
+                if (data.testTrigger) {
+                    createAlert(
+                        new alert(
+                            'test Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alerttest Alert'
+                            , 0
+                        )
+                    )
+                    data.testTrigger = false
                 }
             })
             return { data }
