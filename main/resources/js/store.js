ad912f0efecebe4d2712a78b94001b8892e4e744
YahiroRyo <honakafn@gmail.com>
2021-03-17 15:46:13 +0900

    windowモジュールに使う変数を定義

--- main/resources/js/store.js
+++ main/resources/js/store.js
@@ -3,6 +3,17 @@ import { createStore } from 'vuex'
 export const store = createStore({
     state() {
       return {
+        window: {
+          currentComponent: 0,
+          title: 'タイトルが設定されていません',
+          width: 500,
+          height: 500,
+          use: false,
+          functions: {
+            close: null,
+            open: null,
+          }
+        },
       }
     },
   })
