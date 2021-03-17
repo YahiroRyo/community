fedc6210a9d2a440a9d8ed155baeaffdb1d0e2f1
YahiroRyo <honakafn@gmail.com>
2021-03-17 15:46:38 +0900

    Window内に例として表示するコンポーネントを作成

---
+++ main/resources/js/components/WindowExampleComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">currentComponent 0</div>
+                    <div class="card-header">Window Example Component</div>
+                    <div class="card-header">Width 1000 height 500</div>
+                    <div class="card-body" style="font-size: 50px;">
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { setOpenFunction, setCloseFunction, createWindow, window } from '../window'
+
+    export default {
+        mounted() {
+            createWindow(new window(0, 'WindowExampleComponent', 1000, 500))
+            setOpenFunction(() => {
+                console.log("open")
+            })
+            setCloseFunction(() => {
+                console.log("close")
+            })
+        }
+    }
+</script>
