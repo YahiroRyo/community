94f1df742ccf3f79fd8a5a3473ba52d64acd509a
YahiroRyo <honakafn@gmail.com>
2021-03-18 10:45:46 +0900

    変数の説明参照先を追記

--- main/resources/js/store.js
+++ main/resources/js/store.js
@@ -3,10 +3,12 @@ import { createStore } from 'vuex'
 export const store = createStore({
     state() {
       return {
+        // 変数の説明についてはalert.jsの一行目から参照
         alert: {
           object: null,
           waitIntervals: [],
         },
+        // 変数の説明についてはwindow.jsの一行目から参照
         window: {
           currentComponent: 0,
           title: 'タイトルが設定されていません',
