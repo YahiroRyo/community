6ad051ecf3b985d9a491147594a50a9ce1565632
YahiroRyo <honakafn@gmail.com>
2021-03-19 11:01:20 +0900

    必要なコンポーネントを仮で作成し、パスを通した。

--- main/resources/js/router.js
+++ main/resources/js/router.js
@@ -1,9 +1,33 @@
 //ルーターを設定する
 
-import ***REMOVED*** createRouter, createWebHistory***REMOVED***  from 'vue-router'
+import ***REMOVED*** createRouter, createWebHistory***REMOVED***    from 'vue-router'
+import GlobalPost                           from './components/contents/GlobalPost.vue'
+import Profile                              from './components/contents/Profile.vue'
+import Communities                          from './components/contents/Communities.vue'
+import Contact                              from './components/contents/Contact.vue'
 
 export const router = createRouter(***REMOVED***
     history: createWebHistory(),
     routes: [
+        ***REMOVED***
+            path: '/',
+            name: 'global-post',
+            component: GlobalPost,
+        ***REMOVED***,
+        ***REMOVED***
+            path: '/profile',
+            name: 'profile',
+            component: Profile,
+        ***REMOVED***,
+        ***REMOVED***
+            path: '/communities',
+            name: 'communities',
+            component: Communities,
+        ***REMOVED***,
+        ***REMOVED***
+            path: '/contact',
+            name: 'contact',
+            component: Contact,
+        ***REMOVED***,
     ]
 ***REMOVED***)
