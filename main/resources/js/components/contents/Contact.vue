624be1cee5d1d5ab55649d6f720075cd0d8c9130
YahiroRyo <honakafn@gmail.com>
2021-03-19 23:32:35 +0900

    お問い合わせの画面を作成

--- main/resources/js/components/contents/Contact.vue
+++ main/resources/js/components/contents/Contact.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-        
+        <link rel="stylesheet" href="/css/components/contact/contact.css">
+        <h1 class="contact__title">お問い合わせ</h1>
+        <p class="contact__form-label">お名前</p>
+        <div class="contact__flex">
+            <p>姓</p>
+            <input placeholder="例: 山田" class="contact__form-input">
+            <p>名</p> 
+            <input placeholder="例: 太郎" class="contact__form-input">
+        </div>
+        <div class="contact__flex">
+            <p style="white-space: nowrap">セイ</p>
+            <input placeholder="例: ヤマダ" class="contact__form-input">
+            <p style="white-space: nowrap">メイ</p> 
+            <input placeholder="例: タロウ" class="contact__form-input">
+        </div>
+        <p class="contact__form-label">お問い合わせ内容</p>
+        <textarea placeholder="例: 〇〇について" class="form form_dont-resize height-middle"></textarea>
+        <button disabled class="form form_btn">送信</button>
     </div>
 </template>
 
