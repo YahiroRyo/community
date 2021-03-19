21412084418b8d11ea3f735a05fe5ba2f49d5c9c
YahiroRyo <honakafn@gmail.com>
2021-03-19 17:35:22 +0900

    投稿を様々なテンプレートで用いるため、汎用性のあるように作成

---
+++ main/resources/js/components/Post.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="post">
+        <div class="post__flex">
+            <p class="post__name">***REMOVED******REMOVED***name***REMOVED******REMOVED***</p>
+            <p class="post__font">@***REMOVED******REMOVED***userName***REMOVED******REMOVED***</p>
+        </div>
+        <p class="post__font">***REMOVED******REMOVED***content***REMOVED******REMOVED***</p>
+        <div class="post__flex">
+            <button @click="sendGood(sendGoodKey)" class="post__btn"><img class="post__btn__img" :src="'/images/materials/' + (isGood ? 'clickedHeart.svg' : 'heart.svg')"></button>
+            <p class="post__status">***REMOVED******REMOVED***goodNum***REMOVED******REMOVED***</p>
+            <button class="post__btn"><img class="post__btn__img" src="/images/materials/responce.svg"></button>
+            <p class="post__status">***REMOVED******REMOVED***responceNum***REMOVED******REMOVED***</p>
+            <button @click="" class="post__btn-display-responce">返信を表示する</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default ***REMOVED***
+        props: ***REMOVED***
+            name: ***REMOVED***
+                type: String,
+                required: true,
+            ***REMOVED***,
+            userName: ***REMOVED***
+                type: String,
+                required: true,
+            ***REMOVED***,
+            content: ***REMOVED*** type: String, ***REMOVED***,
+            goodNum: ***REMOVED*** type: Number, ***REMOVED***,
+            responceNum: ***REMOVED*** type: Number, ***REMOVED***,
+            isGood: ***REMOVED*** type: Boolean, ***REMOVED***,
+            sendGood: ***REMOVED*** type: Function, ***REMOVED***,
+            sendGoodKey: ***REMOVED*** type: Number, ***REMOVED***,
+        ***REMOVED***,
+    ***REMOVED***
+</script>
