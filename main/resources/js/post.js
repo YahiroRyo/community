baf1842fbaac94684fe237256a3ed5cd7eaccb8b
YahiroRyo <honakafn@gmail.com>
2021-03-19 17:35:47 +0900

    post classを様々なテンプレートで使うため、jsにまとめた

---
+++ main/resources/js/post.js
@@ -0,0 +1,21 @@
+/* ---------------postクラスについて--------------- */
+// ・変数の説明
+// name:        投稿をした主の名前
+// userName:    投稿をした主のユーザーネーム
+// content:     投稿をした主が投稿した文章
+// isGood:      投稿に対して自分がいいねを押したか
+// goodNum:     投稿のいいね数
+// responceNum: 投稿への返信数
+
+class post ***REMOVED***
+    constructor(name, userName, content, isGood, goodNum, responceNum) ***REMOVED***
+        this.name           = name
+        this.userName       = userName
+        this.content        = content
+        this.isGood         = isGood
+        this.goodNum        = goodNum
+        this.responceNum    = responceNum
+    ***REMOVED***
+***REMOVED***
+
+export ***REMOVED*** post ***REMOVED***
