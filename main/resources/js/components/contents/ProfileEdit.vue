1e136aaf2363209870b3a28ea39cb9aee39a132b
YahiroRyo <honakafn@gmail.com>
2021-03-20 15:25:55 +0900

    profileEditの画面を作成

---
+++ main/resources/js/components/contents/ProfileEdit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <link rel="stylesheet" href="/css/components/profileEdit/profileEdit.css">
+        <h1 class="profile-edit__title">お問い合わせ</h1>
+        <p class="profile-edit__form-label">名前</p>
+        <input class="form">
+        <p class="profile-edit__form-label">ユーザー名</p>
+        <input class="form">
+        <p class="profile-edit__form-label">自己紹介</p>
+        <textarea class="form form_dont-resize"></textarea>
+        <button disabled class="form form_btn">保存</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
