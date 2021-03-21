25d3dcc771390cf27a9e051f7d1fede8748864b0
YahiroRyo <honakafn@gmail.com>
2021-03-22 02:12:48 +0900

    ユーザーの情報を取得後表示するプロフィール画面を作成

--- main/resources/js/components/contents/Profile.vue
+++ main/resources/js/components/contents/Profile.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <link rel="stylesheet" href="/css/components/profile/profile.css">
-        <h1 class="profile__name">name</h1>
+        <h1 class="profile__name">{{data.user.name}}</h1>
         <div class="profile__flex">
-            <p class="profile__user-name">@user-name</p>
+            <p class="profile__user-name">@{{data.user.userName}}</p>
             <router-link to="/profile-edit" class="profile__btn">プロフィールを編集する</router-link>
         </div>
-        <p class="profile__content">content</p>
+        <p class="profile__content">{{data.user.intro}}</p>
         <div class="profile__posts-wapper">
             <template v-for="(post, key) in data.post.objects" :key="key">
                 <Post
@@ -26,7 +26,10 @@
 
 <script>
     import { reactive, onMounted } from 'vue'
+    import { useStore } from 'vuex'
+    import { useRouter } from 'vue-router'
     import { post } from '../../post.js'
+    import axios from 'axios'
     import Post from '../Post.vue'
 
     export default {
@@ -35,6 +38,11 @@
         },
         setup() {
             const data = reactive({
+                user: {
+                    name: '',
+                    userName: '',
+                    intro: '',
+                },
                 post: {
                     objects: [],
                 }
@@ -43,7 +51,25 @@
                 data.post.objects[key].isGood = !data.post.objects[key].isGood
                 data.post.objects[key].isGood ? data.post.objects[key].goodNum++ : data.post.objects[key].goodNum--
             }
+            const getUserData = () => {
+                const userProfileInfos = {
+                    params: {
+                        'uid': localStorage.getItem('uid'),
+                    },
+                }
+                axios.get('/api/get/user-profile', userProfileInfos)
+                .then((responce) => {
+                    data.user.name = responce.data.name
+                    data.user.userName = responce.data.user_name
+                    data.user.intro = responce.data.intro
+                })
+                .catch(() => {
+                   createAlert(new alert('ユーザーデータの取得に失敗しました。', 2))
+                })
+            }
             onMounted(() => {
+                getUserData()
+
                 /* ---------------TODO: サーバーから投稿内容を取得するajax処理を実装--------------- */
 
                 // 仮で表示するために値を格納
