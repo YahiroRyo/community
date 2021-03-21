13cafd1fdeeab29961c00de8579d013465f6259d
YahiroRyo <honakafn@gmail.com>
2021-03-22 01:55:06 +0900

    ログイン機能を実装

--- main/resources/js/components/contents/Login.vue
+++ main/resources/js/components/contents/Login.vue
@@ -1,21 +1,26 @@
 <template>
-    <div>
+    <div @keydown.enter="login">
         <link rel="stylesheet" href="/css/components/login/login.css">
         <h1 class="login__title">ログイン</h1>
         <label class="form-label">メールアドレス</label>
         <input class="form" v-model="data.form.email.content">
         <label class="form-label">パスワード</label>
-        <input class="form" v-model="data.form.password.content">
+        <input type="password" class="form" v-model="data.form.password.content">
         <button @click="login" class="form form_btn">ログイン</button>
     </div>
 </template>
 
 <script>
-    import { reactive, onMounted } from 'vue'
-
+    import { reactive, onMounted }  from 'vue'
+    import { useStore }             from 'vuex'
+    import { useRouter }            from 'vue-router'
+    import firebase                 from 'firebase'
+    
     export default {
         setup() {
             const data = reactive({
+                store: useStore(),
+                router: useRouter(),
                 form: {
                     email: {
                         content: '',
@@ -25,9 +30,29 @@
                     },
                 },
             })
-            const login = () => {
-                /* ---------------TODO: サーバーにログイン情報を投げる--------------- */
-
+            const login = async() => {
+                await firebase.auth().signInWithEmailAndPassword(data.form.email.content, data.form.password.content)
+                .then(async(responce) => {
+                    // uidをローカルストレージに保存
+                    localStorage.setItem('uid', responce.user.uid)
+                    // idTokenを取得
+                    await firebase.auth().currentUser.getIdTokenResult()
+                    .then((idTokenResult) => {
+                        // idTokenをローカルストレージに保存
+                        localStorage.setItem('token', idTokenResult.token)
+                        data.router.push('/')
+                    })
+                    .catch(async() => {
+                        // アクセストークンの取得に失敗した場合はログアウト
+                        createAlert(new alert('アクセストークンの取得に失敗しました。', 2))
+                        await firebase.auth().signOut()
+                        data.router.push('/')
+                    })
+                })
+                .catch(() => {
+                    createAlert(new alert('ログインに失敗しました。', 2))
+                    data.form.password.content = ''
+                })
             }
             return { data, login }
         }
