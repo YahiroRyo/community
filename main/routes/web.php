e786af0f1c9fe8e1152ece92fab9cf5693c3b32e
YahiroRyo <honakafn@gmail.com>
2021-03-22 01:19:19 +0900

    APIに接続できるURLを追加

--- main/routes/web.php
+++ main/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
+Route::post('/api/post/register-user', [UserController::class, 'registerUser']);
 Route::get('/***REMOVED***any?***REMOVED***', function () ***REMOVED***
     return view('index');
 ***REMOVED***)->where('any', '.+');
