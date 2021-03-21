57af771d030c3db5516444f05afa98e8c469ce15
YahiroRyo <honakafn@gmail.com>
2021-03-21 19:17:15 +0900

    ログイン画面を作成

---
+++ main/resources/js/components/contents/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <link rel="stylesheet" href="/css/components/login/login.css">
+        <h1 class="login__title">ログイン</h1>
+        <label class="form-label">メールアドレス</label>
+        <input class="form" v-model="data.form.email.content">
+        <label class="form-label">パスワード</label>
+        <input class="form" v-model="data.form.password.content">
+        <button @click="login" class="form form_btn">ログイン</button>
+    </div>
+</template>
+
+<script>
+    import ***REMOVED*** reactive, onMounted ***REMOVED*** from 'vue'
+
+    export default ***REMOVED***
+        setup() ***REMOVED***
+            const data = reactive(***REMOVED***
+                form: ***REMOVED***
+                    email: ***REMOVED***
+                        content: '',
+                    ***REMOVED***,
+                    password: ***REMOVED***
+                        content: '',
+                    ***REMOVED***,
+                ***REMOVED***,
+            ***REMOVED***)
+            const login = () => ***REMOVED***
+                /* ---------------TODO: サーバーにログイン情報を投げる--------------- */
+
+            ***REMOVED***
+            return ***REMOVED*** data, login ***REMOVED***
+        ***REMOVED***
+    ***REMOVED***
+</script>
