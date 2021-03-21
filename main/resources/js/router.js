31cfac0ff592e0b5677a779695895dc3d56f21a8
YahiroRyo <honakafn@gmail.com>
2021-03-21 18:18:37 +0900

    ルーターにCommuntiyを追加

--- main/resources/js/router.js
+++ main/resources/js/router.js
@@ -4,8 +4,9 @@ import ***REMOVED*** createRouter, createWebHistory***REMOVED***    from 'vue-ro
 import GlobalPost                           from './components/contents/GlobalPost.vue'
 import Profile                              from './components/contents/Profile.vue'
 import Communities                          from './components/contents/Communities.vue'
+import Community                            from './components/contents/Community.vue'
 import Contact                              from './components/contents/Contact.vue'
-import ProfileEdit                              from './components/contents/ProfileEdit.vue'
+import ProfileEdit                          from './components/contents/ProfileEdit.vue'
 
 export const router = createRouter(***REMOVED***
     history: createWebHistory(),
@@ -29,6 +30,11 @@ export const router = createRouter(***REMOVED***
             path: '/communities',
             name: 'communities',
             component: Communities,
+        ***REMOVED***,
+        ***REMOVED***
+            path: '/communities/community/:id',
+            name: 'communities-community',
+            component: Community,
         ***REMOVED***,
         ***REMOVED***
             path: '/contact',
