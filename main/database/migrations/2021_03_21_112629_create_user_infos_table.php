a4cfe53299de53c578c710b96ca3419f0df2e1eb
YahiroRyo <honakafn@gmail.com>
2021-03-21 20:43:40 +0900

    ユーザーの情報を保存するテーブルを作成

---
+++ main/database/migrations/2021_03_21_112629_create_user_infos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+***REMOVED***
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    ***REMOVED***
+        Schema::create('user_infos', function (Blueprint $table) ***REMOVED***
+            $table->integer('user_id')->unsigned();
+            $table->string('name', 30);
+            $table->string('user_name', 30);
+            $table->string('intro', 200)->nullable();
+        ***REMOVED***);
+    ***REMOVED***
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    ***REMOVED***
+        Schema::dropIfExists('user_infos');
+    ***REMOVED***
+***REMOVED***
