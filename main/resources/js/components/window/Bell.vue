52be7aab51520130e2bfe7ef0852a52fec4d74cb
YahiroRyo <honakafn@gmail.com>
2021-03-22 15:52:05 +0900

    仮として通知のwindow画面を作成

---
+++ main/resources/js/components/window/Bell.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="post">
+            <div class="post__flex">
+                <p class="post__font"><strong>〇〇さん</strong>が〇〇コミュニティへ入りたいと加入申請をしてきました。</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { reactive, onMounted } from 'vue'
+    import { setOpenFunction, setCloseFunction, createWindow } from '../../window'
+
+    export default {
+        setup() {
+            const data = reactive({
+
+            })
+            onMounted(() => {
+                createWindow('通知', 1000, 750)
+            })
+            return { data }
+        }
+    }
+</script>
