a6e38244812b46e5d870cd3e91e6577bf329f3ac
YahiroRyo <honakafn@gmail.com>
2021-03-22 15:51:34 +0900

    APIのURLを作成

--- main/routes/web.php
+++ main/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CommunityController;
 Route::post('/api/post/register-user', [UserController::class, 'registerUser']);
 Route::post('/api/post/refresh-user-profile', [UserController::class, 'refreshUserProfile']);
 Route::get('/api/get/user-profile', [UserController::class, 'getUserProfile']);
+Route::get('/api/get/my-user-data', [UserController::class, 'getMyUserData']);
 
 Route::post('/api/post/create-community', [CommunityController::class, 'createCommunity']);
 Route::post('/api/post/can-i-join-community', [CommunityController::class, 'canIJoinCommunity']);
