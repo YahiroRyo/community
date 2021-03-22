5c85eb68734fc5a31e6b46fa39d5d1854073512f
YahiroRyo <honakafn@gmail.com>
2021-03-22 15:57:58 +0900

    通知ボタンを押した場合通知が表示されるよう実装 profileに飛ぶurlを自分のusernameになるように変更

--- main/resources/js/App.vue
+++ main/resources/js/App.vue
@@ -13,7 +13,7 @@
             </li>
             <transition name="router-view-anim">
                 <li v-if="$store.state.user.isLogin" @mouseover="data.menu.profile.isHover = true" @mouseleave="data.menu.profile.isHover = false" :class="{'tab tab_circle': true, 'tab_selecting': data.route.path === '/profile',}">
-                    <router-link class="tab__img" to="/profile">
+                    <router-link class="tab__img" :to="`/profile/${data.menu.profile.userName}`">
                         <img class="tab__img-icon" src="/images/materials/profile.svg">
                     </router-link>
                     <transition name="pop-up-discription-anim">
@@ -42,8 +42,7 @@
         </ul>
         <div class="header__other">
             <ul class="header__other__menu">
-                <!-- <li><router-link class="header__other__menu__item" to="/login">ログイン</router-link></li> -->
-                <!-- <li><router-link class="header__other__menu__item" to="/register">アカウント登録</router-link></li> -->
+                <li @click="displayWindow(3)" class="header__other__menu__item"><img class="header__other__menu__item__btn" src="/images/materials/bell.svg"></li>
             </ul>
         </div>
     </header>
@@ -55,7 +54,7 @@
                 </li>
                 <transition name="router-view-anim">
                     <li v-if="$store.state.user.isLogin">
-                        <router-link to="/profile" class="menu__item"><img class="menu__item-icon" src="/images/materials/profile.svg">プロフィール</router-link>
+                        <router-link :to="`/profile/${data.menu.profile.userName}`" class="menu__item"><img class="menu__item-icon" src="/images/materials/profile.svg">プロフィール</router-link>
                     </li>
                 </transition>
                 <transition name="router-view-anim">
@@ -90,6 +89,7 @@
                         <WindowExample v-if="$store.state.window.currentComponent === 0" />
                         <Modules v-else-if="$store.state.window.currentComponent === 1" />
                         <CreatePost v-else-if="$store.state.window.currentComponent === 2" />
+                        <Bell v-else-if="$store.state.window.currentComponent === 3" />
                     </div>
                 </div>
             </transition>
@@ -129,16 +129,18 @@
 </template>
 
 <script>
-    import { reactive, watch }      from 'vue'
+    import { reactive, watch, onMounted }      from 'vue'
     import { useStore }             from 'vuex'
     import { useRouter, useRoute }  from 'vue-router'
-    import { displayWindow }        from './window';
-    import { alert, createAlert }   from './alert';
+    import { displayWindow }        from './window'
+    import { alert, createAlert }   from './alert'
+    import axios from 'axios'
 
     /* ---------------コンポーネントインポート--------------- */
     import WindowExample from './components/window/WindowExampleComponent.vue'
     import Modules from './components/window/Modules.vue'
     import CreatePost from './components/window/CreatePost.vue'
+    import Bell from './components/window/Bell.vue'
 
     export default {
         components:{
@@ -149,6 +151,7 @@
             WindowExample,
             Modules,
             CreatePost,
+            Bell,
         },
         setup() {
             const data = reactive({
@@ -161,7 +164,7 @@
                 },
                 menu: {
                     home: { isHover: false, },
-                    profile: { isHover: false, },
+                    profile: { isHover: false, userName: '', },
                     communities: { isHover: false, },
                     contact: { isHover: false, },
                 },
@@ -206,6 +209,24 @@
                     createAlert(new alert('alert', 0))
                     data.testTrigger = false
                 }
+            })
+            watch(() => data.store.state.user.profileUpdate, () => {
+                if (data.store.state.user.profileUpdate) {
+                    data.store.state.user.profileUpdate = false
+                    data.menu.profile.userName = localStorage.getItem('myUserName')
+                }
+            })
+            onMounted(() => {
+                const myUserDataInfos = {
+                    params: {
+                        uid: localStorage.getItem('uid'),
+                    }
+                }
+                axios.get('/api/get/my-user-data', myUserDataInfos)
+                .then((responce) => {
+                    data.menu.profile.userName = responce.data.user_name
+                    localStorage.setItem('myUserName', responce.data.user_name)
+                })
             })
             return { displayWindow, data }
         }
