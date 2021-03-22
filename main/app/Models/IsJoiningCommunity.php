6beb57507ecccc929abaa4fc247f5d27cd246781
YahiroRyo <honakafn@gmail.com>
2021-03-22 15:20:41 +0900

    コミュニティに加入しているモデルを作成

---
+++ main/app/Models/IsJoiningCommunity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IsJoiningCommunity extends Model
+***REMOVED***
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id',
+        'community_id',
+    ];
+***REMOVED***
