aba3d0282eb1ffec2f3956db88877525c4e5ad6b
YahiroRyo <honakafn@gmail.com>
2021-03-22 15:57:15 +0900

    urlパラメータに変更が加わった場合、それを検知しデータを取得し直す。 自分のprofileだった場合、プロフィールを編集ボタンを表示するよう実装。

--- main/resources/js/components/contents/Profile.vue
+++ main/resources/js/components/contents/Profile.vue
@@ -4,7 +4,7 @@
         <h1 class="profile__name">***REMOVED******REMOVED***data.user.name***REMOVED******REMOVED***</h1>
         <div class="profile__flex">
             <p class="profile__user-name">@***REMOVED******REMOVED***data.user.userName***REMOVED******REMOVED***</p>
-            <router-link to="/profile-edit" class="profile__btn">プロフィールを編集する</router-link>
+            <router-link to="/profile-edit" v-if="data.myUserName === data.user.userName" class="profile__btn">プロフィールを編集する</router-link>
         </div>
         <p class="profile__content">***REMOVED******REMOVED***data.user.intro***REMOVED******REMOVED***</p>
         <div class="profile__posts-wapper">
@@ -25,9 +25,9 @@
 </template>
 
 <script>
-    import ***REMOVED*** reactive, onMounted ***REMOVED*** from 'vue'
+    import ***REMOVED*** reactive, onMounted, onUpdated ***REMOVED*** from 'vue'
     import ***REMOVED*** useStore ***REMOVED*** from 'vuex'
-    import ***REMOVED*** useRouter ***REMOVED*** from 'vue-router'
+    import ***REMOVED*** useRouter, useRoute ***REMOVED*** from 'vue-router'
     import ***REMOVED*** post ***REMOVED*** from '../../post.js'
     import axios from 'axios'
     import Post from '../Post.vue'
@@ -38,11 +38,13 @@
         ***REMOVED***,
         setup() ***REMOVED***
             const data = reactive(***REMOVED***
+                route: useRoute(),
                 user: ***REMOVED***
                     name: '',
                     userName: '',
                     intro: '',
                 ***REMOVED***,
+                myUserName: localStorage.getItem('myUserName'),
                 post: ***REMOVED***
                     objects: [],
                 ***REMOVED***
@@ -54,7 +56,7 @@
             const getUserData = () => ***REMOVED***
                 const userProfileInfos = ***REMOVED***
                     params: ***REMOVED***
-                        'uid': localStorage.getItem('uid'),
+                        'userName': data.route.params.userName,
                     ***REMOVED***,
                 ***REMOVED***
                 axios.get('/api/get/user-profile', userProfileInfos)
@@ -67,11 +69,14 @@
                    createAlert(new alert('ユーザーデータの取得に失敗しました。', 2))
                 ***REMOVED***)
             ***REMOVED***
+            onUpdated(() => ***REMOVED***
+                getUserData()
+            ***REMOVED***)
             onMounted(() => ***REMOVED***
                 getUserData()
 
                 /* ---------------TODO: サーバーから投稿内容を取得するajax処理を実装--------------- */
-
+                
                 // 仮で表示するために値を格納
                 for (let i = 0; i < 100; i++)
                     data.post.objects.push(new post('name', 'userName', 'content', true, 1, 5))
