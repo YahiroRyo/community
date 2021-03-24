5649c1d39e98e12a0ea93243ca960ca2fd2928bd
YahiroRyo <honakafn@gmail.com>
2021-03-24 14:20:37 +0900

    userIdのbellを取得できるメソッドを実装

---
+++ main/app/Http/Controllers/BellController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Bell;
+use App\Models\User;
+use App\Models\CanIJoinCommunity;
+
+class BellController extends Controller
+{
+    public function getBells(Request $request) {
+        // $request->uid
+        // $request->take
+        // $request->gotNum
+        if (ctype_digit(strval($request->take)) && (ctype_digit(strval($request->gotNum)) || !$request->gotNum)) {
+            $result = [];
+
+            $take = intval($request->take);
+            $gotNum = intval($request->gotNum);
+            
+            $userId = User::where('uid', $request->uid)->first()['id'];
+            $bells = Bell::where('user_id', $userId)->take($take + $gotNum)->get(['id', 'type', 'user_id']);
+            for ($i = $gotNum; $i < count($bells); $i++) {
+                if ($bells[$i]['type'] === 1) {
+                    $bells[$i]['dataForType'] = CanIJoinCommunity::where('bell_id', $bells[$i]['id'])->with(['community' => function($query) {
+                        $query->select(['id', 'name']);
+                    }, 'userInfo' => function($query) {
+                        $query->select(['name', 'user_name', 'user_id']);
+                    }])->first();
+                }
+                array_push($result, $bells[$i]);
+            }
+            return $result;
+        }
+    }
+}
