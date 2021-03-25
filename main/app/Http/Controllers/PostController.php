407ee7a08b304b8c79d037a2f301e57fb8480bdb
YahiroRyo <honakafn@gmail.com>
2021-03-26 00:21:08 +0900

    投稿にいいねといいね数を載せて返すように実装 投稿にいいねできる機能を作成

--- main/app/Http/Controllers/PostController.php
+++ main/app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use Kreait\Firebase\Auth;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Great;
 
 class PostController extends Controller
 {
@@ -57,23 +58,63 @@ class PostController extends Controller
     public function getGlobalPosts(Request $request): array {
         // $request->take
         // $request->gotNum
+        // $request->uid
         if (ctype_digit(strval($request->take)) && (ctype_digit(strval($request->gotNum)) || !$request->gotNum)) {
             $result = [];
             
             $take   = intval($request->take);
             $gotNum = intval($request->gotNum);
 
+            $userId = User::where('uid', $request->uid)->first()['id'];
             $posts  = Post::select(['content', 'id', 'user_id'])
                             ->take($take + $gotNum)
-                            ->with(['userInfo' => function ($query) {
-                                $query->select(['name', 'user_name', 'user_id']);
-                            }])
+                            ->with([
+                                'userInfo' => function ($query) {
+                                    $query->select(['name', 'user_name', 'user_id']);
+                                },
+                                'isGreatPost' => function ($query) use ($userId) {
+                                    $query->where('user_id', $userId);
+                                },
+                                'greatPostNum',
+                            ])
                             ->orderBy('id', 'desc')
                             ->get();
             for ($i = $gotNum; $i < count($posts); $i++) {
                 array_push($result, $posts[$i]);
             }
             return $result;
+        }
+    }
+    public function greatPost(Request $request) {
+        // $request->uid
+        // $request->token
+        // $request->postId
+        if ($this->isNormalToken($request->token)) {
+            $userId = User::where('uid', $request->uid)->first()['id'];
+            $isGreatExists = Great::where('user_id', $userId)
+                                    ->where('post_id',$request->postId)
+                                    ->exists();
+            if ($isGreatExists) {
+                Great::where('user_id', $userId)
+                        ->where('post_id',$request->postId)
+                        ->delete();
+            } else {
+                $great = new Great;
+                $great->fill([
+                    'user_id' => $userId,
+                    'post_id' => $request->postId,
+                ]);
+                $great->save();
+            }
+            return [
+                'isNormalToken' => true,
+                'isGreat' => true,
+            ];
+        } else {
+            return [
+                'isNormalToken' => false,
+                'isGreat' => false,
+            ];
         }
     }
 }
