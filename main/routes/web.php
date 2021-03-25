b09e3d9b89c5e2a1f258e306c476c532e505d236
YahiroRyo <honakafn@gmail.com>
2021-03-25 23:08:33 +0900

    APIのURLを作成 可読性が上がるように縦で揃えた

--- main/routes/web.php
+++ main/routes/web.php
@@ -2,23 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\BellController;
 use App\Http\Controllers\CommunityController;
+use App\Http\Controllers\BellController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
-Route::get('/api/get/bells', [BellController::class, 'getBells']);
+Route::get('/api/get/bells',                    [BellController::class, 'getBells']);
 
-Route::post('/api/post/create-community', [CommunityController::class, 'createCommunity']);
-Route::post('/api/post/can-i-join-community', [CommunityController::class, 'canIJoinCommunity']);
-Route::post('/api/post/join-community', [CommunityController::class, 'joinCommunity']);
-Route::post('/api/post/dont-join-community', [CommunityController::class, 'dontJoinCommunity']);
-Route::post('/api/post/cancel-join-community', [CommunityController::class, 'cancelJoinCommunity']);
-Route::get('/api/get/communities', [CommunityController::class, 'getCommunities']);
+Route::post('/api/post/cancel-join-community',  [CommunityController::class, 'cancelJoinCommunity']);
+Route::post('/api/post/can-i-join-community',   [CommunityController::class, 'canIJoinCommunity']);
+Route::post('/api/post/dont-join-community',    [CommunityController::class, 'dontJoinCommunity']);
+Route::post('/api/post/create-community',       [CommunityController::class, 'createCommunity']);
+Route::post('/api/post/join-community',         [CommunityController::class, 'joinCommunity']);
+Route::get('/api/get/communities',              [CommunityController::class, 'getCommunities']);
 
-Route::post('/api/post/register-user', [UserController::class, 'registerUser']);
-Route::post('/api/post/refresh-user-profile', [UserController::class, 'refreshUserProfile']);
-Route::get('/api/get/user-profile', [UserController::class, 'getUserProfile']);
-Route::get('/api/get/my-user-data', [UserController::class, 'getMyUserData']);
+Route::post('/api/post/refresh-user-profile',   [UserController::class, 'refreshUserProfile']);
+Route::post('/api/post/register-user',          [UserController::class, 'registerUser']);
+Route::get('/api/get/user-profile',             [UserController::class, 'getUserProfile']);
+Route::get('/api/get/my-user-data',             [UserController::class, 'getMyUserData']);
+
+Route::post('/api/post/create-post',            [PostController::class, 'createPost']);
+Route::get('/api/get/global-posts',             [PostController::class, 'getGlobalPosts']);
 
 Route::get('/{any?}', function () {
     return view('index');
