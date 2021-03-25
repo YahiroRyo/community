794ab9adfe0e9accc04c18e288c696d6d916ffe3
YahiroRyo <honakafn@gmail.com>
2021-03-25 23:03:07 +0900

    投稿を作成するコントローラーを作成

---
+++ main/app/Http/Controllers/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Kreait\Firebase\Auth;
+
+use App\Models\Post;
+use App\Models\User;
+
+class PostController extends Controller
+***REMOVED***
+    private $auth;
+
+    public function __construct(Auth $auth)
+    ***REMOVED***
+        $this->auth = $auth;
+    ***REMOVED***
+    // トークンが正常なものか確認
+    public function isNormalToken($token): bool ***REMOVED***
+        try ***REMOVED***
+            $isCorrectToken = $this->auth->verifyIdToken($token);
+            return true;
+        ***REMOVED*** catch (\Exception $e) ***REMOVED***
+            return false;
+        ***REMOVED***
+    ***REMOVED***
+    // 投稿作成
+    public function createPost(Request $request) ***REMOVED***
+        // $request->uid
+        // $request->token
+        // $request->content
+        if ($this->isNormalToken($request->token) && strlen($request->content) <= 200) ***REMOVED***
+            $userId = User::where('uid', $request->uid)->first()['id'];
+            $post = new Post;
+            $post->fill([
+                'user_id' => $userId,
+                'post_id' => null,
+                'content' => $request->content,
+            ]);
+            $post->save();
+            return [
+                'isNormalToken' => true,
+                'isCreatePost' => true,
+            ];
+        ***REMOVED*** else ***REMOVED***
+            return [
+                'isNormalToken' => false,
+                'isCreatePost' => false,
+            ];
+        ***REMOVED***
+    ***REMOVED***
+    public function createResponcePost(Request $request) ***REMOVED***
+        // TODO: 返信を作成するPostを作成
+    ***REMOVED***
+    public function getGlobalPosts(Request $request): array ***REMOVED***
+        // $request->take
+        // $request->gotNum
+        if (ctype_digit(strval($request->take)) && (ctype_digit(strval($request->gotNum)) || !$request->gotNum)) ***REMOVED***
+            $result = [];
+            
+            $take   = intval($request->take);
+            $gotNum = intval($request->gotNum);
+
+            $posts  = Post::select(['content', 'id', 'user_id'])
+                            ->take($take + $gotNum)
+                            ->with(['userInfo' => function ($query) ***REMOVED***
+                                $query->select(['name', 'user_name', 'user_id']);
+                            ***REMOVED***])
+                            ->orderBy('id', 'desc')
+                            ->get();
+            for ($i = $gotNum; $i < count($posts); $i++) ***REMOVED***
+                array_push($result, $posts[$i]);
+            ***REMOVED***
+            return $result;
+        ***REMOVED***
+    ***REMOVED***
+***REMOVED***
