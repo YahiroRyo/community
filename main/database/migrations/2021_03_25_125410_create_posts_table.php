84f1b8acd489a50a1817166348d9ee361684a139
YahiroRyo <honakafn@gmail.com>
2021-03-25 23:02:01 +0900

    投稿を保存するテーブルを作成

---
+++ main/database/migrations/2021_03_25_125410_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+***REMOVED***
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    ***REMOVED***
+        Schema::create('posts', function (Blueprint $table) ***REMOVED***
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned()->nullable();
+            $table->string('content', 200);
+            $table->timestamps();
+        ***REMOVED***);
+    ***REMOVED***
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    ***REMOVED***
+        Schema::dropIfExists('posts');
+    ***REMOVED***
+***REMOVED***
