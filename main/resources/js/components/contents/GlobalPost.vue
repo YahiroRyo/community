e8a526cf7e252a77ca2be0032d3f81b52f981f9a
YahiroRyo <honakafn@gmail.com>
2021-03-25 23:06:52 +0900

    ツイートをサーバーに取り表示する機能を実装

--- main/resources/js/components/contents/GlobalPost.vue
+++ main/resources/js/components/contents/GlobalPost.vue
@@ -16,30 +16,68 @@
 </template>
 
 <script>
-    import ***REMOVED*** reactive, onMounted ***REMOVED***  from 'vue'
-    import ***REMOVED*** post ***REMOVED***                 from '../../post.js'
-    import Post                     from '../Post.vue'
+    import ***REMOVED*** addPageEvent, removeAtAllFunc ***REMOVED***    from '../../page.js'
+    import ***REMOVED*** reactive, onMounted ***REMOVED***              from 'vue'
+    import ***REMOVED*** getUidAndToken ***REMOVED***                   from '../../supportFirebase.js'
+    import ***REMOVED*** post ***REMOVED***                             from '../../post.js'
+    import axios                                from 'axios'
+    import Post                                 from '../Post.vue'
 
     export default ***REMOVED***
         components: ***REMOVED*** Post, ***REMOVED***,
         setup() ***REMOVED***
             const data = reactive(***REMOVED***
                 post: ***REMOVED***
-                    objects: [],
+                    cantGetPosts:   false,
+                    objects:        [],
+                    gotNum:         0,
+                    take:           50,
                 ***REMOVED***
             ***REMOVED***)
             const sendGood = (key) => ***REMOVED***
                 data.post.objects[key].isGood = !data.post.objects[key].isGood
                 data.post.objects[key].isGood ? data.post.objects[key].goodNum++ : data.post.objects[key].goodNum--
             ***REMOVED***
+            const getPosts = async() => ***REMOVED***
+                if (!data.post.cantGetPosts) ***REMOVED***
+                    const user = await getUidAndToken()
+                    const globalPostsInfos = ***REMOVED***
+                        params: ***REMOVED***
+                            gotNum: data.post.gotNum,
+                            take:   data.post.take,
+                            uid:    user.uid,
+                        ***REMOVED***
+                    ***REMOVED***
+                    axios.get('/api/get/global-posts', globalPostsInfos)
+                    .then((responce) => ***REMOVED***
+                        console.log(responce)
+                        data.post.gotNum += data.post.take
+                        if (data.post.take > responce.data.length)
+                            data.post.cantGetPosts = true
+                        responce.data.forEach((obj) => ***REMOVED***
+                            data.post.objects.push(
+                                new post(
+                                    obj.user_info.name,
+                                    obj.user_info.user_name,
+                                    obj.content,
+                                    false,
+                                    0,
+                                    0,
+                                )
+                            )
+                        ***REMOVED***)
+                    ***REMOVED***)
+                ***REMOVED***
+            ***REMOVED***
             onMounted(() => ***REMOVED***
-                /* ---------------TODO: サーバーから投稿内容を取得するajax処理を実装--------------- */
-
-                // 仮で表示するために値を格納
-                for (let i = 0; i < 100; i++)
-                    data.post.objects.push(new post('name', 'userName', 'content', true, 1, 5))
+                getPosts()
+                addPageEvent('pageMostBottom', () => ***REMOVED***getPosts()***REMOVED***)
             ***REMOVED***)
             return ***REMOVED*** data, sendGood ***REMOVED***
+        ***REMOVED***,
+        beforeRouteLeave (to, from, next) ***REMOVED***
+            removeAtAllFunc()
+            next()
         ***REMOVED***
     ***REMOVED***
 </script>
