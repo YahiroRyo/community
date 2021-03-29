c3d6f07d0f1ec76cd05f7b699eda5bc1f20026df
YahiroRyo <honakafn@gmail.com>
2021-03-29 14:14:32 +0900

    community_idを追加したためfullableにcommunity_idを追加

--- main/app/Models/Post.php
+++ main/app/Models/Post.php
@@ -14,6 +14,7 @@ class Post extends Model
     protected $fillable = [
         'user_id',
         'post_id',
+        'community_id',
         'content',
     ];
     public function userInfo() ***REMOVED***
