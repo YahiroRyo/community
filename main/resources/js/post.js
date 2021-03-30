f9253431f37b8344faa2200f9f5b1443f16c519a
YahiroRyo <honakafn@gmail.com>
2021-03-30 23:19:04 +0900

    sendGoodでログインしていない場合の処理を追加

--- main/resources/js/post.js
+++ main/resources/js/post.js
@@ -9,6 +9,8 @@
 // postId:      投稿のID
 import { createAlert, alert, notNormalTokenAlert }  from './alert.js'
 import { getUidAndToken }                           from './supportFirebase.js'
+import { displayWindow }                            from './window.js'
+import { store }                                    from './store.js'
 import axios                                        from 'axios' 
 
 class post {
@@ -24,25 +26,29 @@ class post {
     }
 }
 const sendGood = async(postObj) => {
-    const user = await getUidAndToken()
-    const greatPostInfos = {
-        postId: postObj.postId,
-        token:  user.token,
-        uid:    user.uid,
-    }
-    axios.post('/api/post/great-post', greatPostInfos)
-    .then((responce) => {
-        if (responce.data.isNormalToken) {
-            if (responce.data.isGreat) {
-                postObj.isGood = !postObj.isGood
-                postObj.isGood ? postObj.goodNum++ : postObj.goodNum--
-            } else {
-                createAlert(new alert('いいねすることができませんでした。', 2))
-            }
-        } else {
-            notNormalTokenAlert()
+    if (store.state.user.isLogin) {
+        const user = await getUidAndToken()
+        const greatPostInfos = {
+            postId: postObj.postId,
+            token:  user.token,
+            uid:    user.uid,
         }
-    })
+        axios.post('/api/post/great-post', greatPostInfos)
+        .then((responce) => {
+            if (responce.data.isNormalToken) {
+                if (responce.data.isGreat) {
+                    postObj.isGood = !postObj.isGood
+                    postObj.isGood ? postObj.goodNum++ : postObj.goodNum--
+                } else {
+                    createAlert(new alert('いいねすることができませんでした。', 2))
+                }
+            } else {
+                notNormalTokenAlert()
+            }
+        })
+    } else {
+        displayWindow(5)
+    }
 }
 
 export { post, sendGood }
