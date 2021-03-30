82ca7f69427da9ede01a752fb36c4135f664f6e3
YahiroRyo <honakafn@gmail.com>
2021-03-30 23:19:04 +0900

    sendGoodでログインしていない場合の処理を追加

--- main/resources/js/post.js
+++ main/resources/js/post.js
@@ -9,6 +9,8 @@
 // postId:      投稿のID
 import ***REMOVED*** createAlert, alert, notNormalTokenAlert ***REMOVED***  from './alert.js'
 import ***REMOVED*** getUidAndToken ***REMOVED***                           from './supportFirebase.js'
+import ***REMOVED*** displayWindow ***REMOVED***                            from './window.js'
+import ***REMOVED*** store ***REMOVED***                                    from './store.js'
 import axios                                        from 'axios' 
 
 class post ***REMOVED***
@@ -24,25 +26,29 @@ class post ***REMOVED***
     ***REMOVED***
 ***REMOVED***
 const sendGood = async(postObj) => ***REMOVED***
-    const user = await getUidAndToken()
-    const greatPostInfos = ***REMOVED***
-        postId: postObj.postId,
-        token:  user.token,
-        uid:    user.uid,
-    ***REMOVED***
-    axios.post('/api/post/great-post', greatPostInfos)
-    .then((responce) => ***REMOVED***
-        if (responce.data.isNormalToken) ***REMOVED***
-            if (responce.data.isGreat) ***REMOVED***
-                postObj.isGood = !postObj.isGood
-                postObj.isGood ? postObj.goodNum++ : postObj.goodNum--
-            ***REMOVED*** else ***REMOVED***
-                createAlert(new alert('いいねすることができませんでした。', 2))
-            ***REMOVED***
-        ***REMOVED*** else ***REMOVED***
-            notNormalTokenAlert()
+    if (store.state.user.isLogin) ***REMOVED***
+        const user = await getUidAndToken()
+        const greatPostInfos = ***REMOVED***
+            postId: postObj.postId,
+            token:  user.token,
+            uid:    user.uid,
         ***REMOVED***
-    ***REMOVED***)
+        axios.post('/api/post/great-post', greatPostInfos)
+        .then((responce) => ***REMOVED***
+            if (responce.data.isNormalToken) ***REMOVED***
+                if (responce.data.isGreat) ***REMOVED***
+                    postObj.isGood = !postObj.isGood
+                    postObj.isGood ? postObj.goodNum++ : postObj.goodNum--
+                ***REMOVED*** else ***REMOVED***
+                    createAlert(new alert('いいねすることができませんでした。', 2))
+                ***REMOVED***
+            ***REMOVED*** else ***REMOVED***
+                notNormalTokenAlert()
+            ***REMOVED***
+        ***REMOVED***)
+    ***REMOVED*** else ***REMOVED***
+        displayWindow(5)
+    ***REMOVED***
 ***REMOVED***
 
 export ***REMOVED*** post, sendGood ***REMOVED***
