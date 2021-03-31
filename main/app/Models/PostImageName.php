58e81da286e74566d065999b3d055f07f1dc95e3
YahiroRyo <honakafn@gmail.com>
2021-03-31 20:51:52 +0900

    投稿に画像を紐づけるためのテーブルのモデルを作成

---
+++ main/app/Models/PostImageName.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostImageName extends Model
+***REMOVED***
+    public $timestamps  = false;
+    protected $fillable = [
+        'post_id',
+        'image_name',
+    ];
+***REMOVED***
