3746a7eccbee75773494200f3609a9d8cd7ab355
YahiroRyo <honakafn@gmail.com>
2021-04-01 17:31:03 +0900

    画像をクリックした場合の処理を追加

--- main/resources/js/components/Post.vue
+++ main/resources/js/components/Post.vue
@@ -9,7 +9,7 @@
                 </div>
                 <p class="post__font">***REMOVED******REMOVED***content***REMOVED******REMOVED***</p>
                 <div class="post__img-wapper" v-show="postImageNames.length !== 0">
-                    <div class="post__img" v-for="(postImageName, key) in postImageNames" :style="`background-image: url(/storage/$***REMOVED***postImageName.image_name***REMOVED***)`" :key="key"></div>
+                    <div class="post__img"  v-for="(postImageName, key) in postImageNames" @click="displayImage(key)" :style="`background-image: url(/storage/$***REMOVED***postImageName.image_name***REMOVED***)`" :key="key"></div>
                 </div>
                 <div class="post__flex">
                     <button @click="sendGood(sendArg)" class="post__btn"><img class="post__btn__img" :src="'/images/materials/' + (isGood ? 'clickedHeart.svg' : 'heart.svg')"></button>
@@ -72,7 +72,12 @@
                     displayWindow(5)
                 ***REMOVED***
             ***REMOVED***
-            return ***REMOVED*** data, responceToPost ***REMOVED***
+            const displayImage = (key) => ***REMOVED***
+                data.store.state.post.image = props.postImageNames
+                data.store.state.post.imageKey = key
+                displayWindow(6)
+            ***REMOVED***
+            return ***REMOVED*** data, responceToPost, displayImage ***REMOVED***
         ***REMOVED***
     ***REMOVED***
 </script>
